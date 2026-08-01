67b72fe943ebdaec44f4505d91c7472161accd1e
Aayushi262005 <aayushinishisharma@gmail.com>
2026-08-02 01:50:48 +0530

    added back to home in the login page

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link,useNavigate} from 'react-router-dom'
+import signupbg from '../assets/Signbg.png'
 
 const Signup = () => {
     const navigate= useNavigate();
